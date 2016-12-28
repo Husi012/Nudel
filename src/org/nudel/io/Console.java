e5dd88bf9a3900729e0eeaba790ff77f7a414e8f
streifi <sebastian.weiermann@yahoo.com>
2016-12-28 13:59:32 +0100

    base layout

---
+++ src/org/nudel/io/Console.java
@@ -0,0 +1,18 @@
+package org.nudel.io;
+
+import org.nudel.io.std.StdErr;
+import org.nudel.io.std.StdIn;
+import org.nudel.io.std.StdOut;
+
+/**
+ * Created by streifi on 28/12/16.
+ */
+public class Console {
+    public static StdIn in;
+    public static StdOut out;
+    public static StdErr err;
+
+    public Console() {
+        //TODO
+    }
+}
---
+++ src/org/nudel/io/std/StdErr.java
@@ -0,0 +1,7 @@
+package org.nudel.io.std;
+
+/**
+ * Created by streifi on 28/12/16.
+ */
+public class StdErr extends StdWriter {
+}
